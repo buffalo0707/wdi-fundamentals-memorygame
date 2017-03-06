b7a1efce0e23907ae773ac80dee69bf8083adb98
Alan Henderson <alanhenderson@Alans-MacBook-Pro.local>
2017-03-06 11:13:25 -0500

    Unit 8 assignment

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,7 @@
-console.log("JS file is connected to HTML! Woo!")
+console.log("JS file is connected to HTML! Woo!");
+
+//Create variables for each card.
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
