d6f8d819cc00a5b7075f46de76db972178481522
Alan Henderson <alanhenderson@Alans-MacBook-Pro.local>
2017-03-06 11:47:35 -0500

    Unit 9 Exercise

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,3 +5,11 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+if (cardOne === cardFour) {
+	alert("Sorry, try agqin.");
+} else if (cardThree === cardFour) {
+	alert("You found a match!");
+} else if (cardOne === cardTwo) {
+	alert("You found a match!");
+}
