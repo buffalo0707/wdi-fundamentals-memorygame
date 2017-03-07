0bf1b97c0c73a37e1b9a6edca2d186a3a0f0d102
Alan Henderson <alanhenderson@Alans-MacBook-Pro.local>
2017-03-07 10:21:01 -0500

    Unit 10 assignment

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,4 +1,3 @@
-console.log("JS file is connected to HTML! Woo!");
 
 //Create variables for each card.
 var cardOne = "queen";
@@ -6,10 +5,29 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-if (cardOne === cardFour) {
-	alert("Sorry, try agqin.");
-} else if (cardThree === cardFour) {
-	alert("You found a match!");
-} else if (cardOne === cardTwo) {
-	alert("You found a match!");
+//Compare card values to eachother and alert user
+// if (cardOne === cardFour) {
+// 	alert("Sorry, try again.");
+// } else if (cardThree === cardFour) {
+// 	alert("You found a match!");
+// } else if (cardOne === cardTwo) {
+// 	alert("You found a match!");
+// }
+
+//Function for creating cards. Will create the number of cards passed in as parameter
+var createCards = function(a){
+	//For each card
+	for (var i = 1; i <= a; i++){
+		//Add a new div element
+		var newCard = document.createElement('div');
+		//with class of 'card'
+		newCard.className = 'card';
+		//add new element to the element with id of 'game-board'
+		document.getElementById('game-board').appendChild(newCard);
+	}
 }
+
+//Variable to number of cards
+var numCards = 4
+//Run function to create cards
+createCards(numCards);
