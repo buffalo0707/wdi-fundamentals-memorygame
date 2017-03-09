0d48b3a6bb52a5a326b1434739a67f1b00c97272
Alan Henderson <alan.henderson@libertymutual.com>
2017-03-09 08:30:00 -0500

    Post Unit 11 enhancements: Added reset button, updated hover CSS, and added webfonts.

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,27 +1,20 @@
 
-var cards = ['queen', 'queen', 'king', 'king'];
+var cards = ['queen', 'queen', 'king', 'king','queen', 'queen', 'king', 'king','queen', 'queen', 'king', 'king',];
 var cardsInPlay = [];
 var score = 0
 
-//Compare card values to eachother and alert user
-// if (cardOne === cardFour) {
-// 	alert("Sorry, try again.");
-// } else if (cardThree === cardFour) {
-// 	alert("You found a match!");
-// } else if (cardOne === cardTwo) {
-// 	alert("You found a match!");
-// }
-
-//Function for creating cards. Will create the number of cards passed in as parameter
-var createCards = function(a) {
-	//For each card
+// Function for creating cards. Will create the number of cards passed in as parameter
+var createBoard = function(a) {
+	// shuffle cards
+	cards.sort(function(a, b){return 0.5 - Math.random()});
+	// For each card
 	for (var i = 0; i < a; i++){
 		//Add a new div element
 		var newCard = document.createElement('div');
-		//with class of 'card'
+		// with class of 'card'
 		newCard.className = 'card';
 		newCard.setAttribute('data-card', cards[i]);
-		//add new element to the element with id of 'game-board'
+		// add new element to the element with id of 'game-board'
 		document.getElementById('game-board').appendChild(newCard);
 		newCard.addEventListener('click', isTwoCards)
 	}
@@ -30,16 +23,15 @@ var createCards = function(a) {
 var isTwoCards = function() {
   // add card to array of cards in play
   cardsInPlay.push(this.getAttribute('data-card'));
-	// show the card's image
+	// show the card's image (flip face up)
 	console.log(this.getAttribute('data-card'));
 	if (this.getAttribute('data-card') === 'king') {
 		this.innerHTML = "<img src='http://i.imgur.com/bnuv5Im.png'>"; // king
 	} else {
 		this.innerHTML = "<img src='http://i.imgur.com/v6buNt2.png'>"; //queen
 	}
-
   // if you have two cards in play, check for a match
-  if (cardsInPlay.length === 2) {
+  	if (cardsInPlay.length === 2) {
 
     // pass the cardsInPlay as an argument to the isMatch function
     isMatch(cardsInPlay);
@@ -47,29 +39,38 @@ var isTwoCards = function() {
     // clear cards in play array for your next try
     cardsInPlay = [];
 	}
-  }
+ }
 
+// check to see if cards are a match
 var isMatch = function(cardsInPlay) {
+	// if cards are a match
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("That's a match - you win!");
+		// display message that cards match
+		alert("That's a match - +1 internets for you!");
+		// increment score total by 1
 		score += 1;
-		console.log(score)
+		// update score element with new score
 		document.getElementById('score').innerHTML = "Score: " + score
 	} else {
+		// if cards do not match, display message of complete failure
 		alert("Not a match... Try again!");
 	}
-	for (var i = 0; i < this.length; i++) {
-		this.innerHTML = "";
-	}
-
 }
 
-//Run function to create cards
-createCards(cards.length);
+// Run function to create cards
+createBoard(cards.length);
 
+// When Reset button is clicked
 document.getElementById("reset").addEventListener("click", function(){
-    var cardsInPlay = document.getElementsByClassName('card');
+	//find all cards on the board
+	var cardsInPlay = document.getElementsByClassName('card');
+    //loop through each card on the board
     for (var i = 0; i < cardsInPlay.length; i++) {
+   		//remove image from card (flip face down)
     	cardsInPlay[i].innerHTML = "";
     }
+    // set score variable back to 0
+	score = 0;
+	// set score display to equal variable, which is 0
+	document.getElementById('score').innerHTML = "Score: " + score;
 });
