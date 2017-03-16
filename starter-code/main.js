21eb5b9d3c227c950b0d0c2a561c431f51e889f8
Alan Henderson <alan.henderson@libertymutual.com>
2017-03-16 10:20:24 -0400

    Added a rudimentary blackjack game. Updated card images with files stored in local assets folder.

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,12 +1,14 @@
 
 var cards = ['ace', 'king', 'queen', 'jack', 'ten', 'joker', 'ace', 'king', 'queen', 'jack', 'ten', 'joker',];
-var cardsInPlay = [];
-var score = 0
-var flippedCards = 0
+var cardsInPlay = []; //memory game selected cards
+var score = 0; // memory game score
+var flippedCards = 0; // counter for number of face up cards in memory game
+var blackjackCards = []; // array for blackjack deck
+var cardTotal = 0; // sum of dealt blackjack cards
 
 // Function for creating cards. Will create the number of cards passed in as parameter
 var createBoard = function(a) {
-	// shuffle cards
+	// shuffle cards with a sort function I found somewhere on the internet
 	cards.sort(function(a, b){return 0.5 - Math.random()});
 	// For each card
 	for (var i = 0; i < a; i++){
@@ -19,60 +21,59 @@ var createBoard = function(a) {
 		// show all cards face up
 		if (newCard.getAttribute('data-card') === 'king') {
 			// king
-			newCard.innerHTML = "<img src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/06/King_of_hearts2.svg/2000px-King_of_hearts2.svg.png'>";
+			newCard.innerHTML = "<img src='assets/king_of_hearts.png'>";
 		} else if (newCard.getAttribute('data-card') === 'queen') {
 			// queen
-			newCard.innerHTML = "<img src='https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/Queen_of_hearts2.svg/2000px-Queen_of_hearts2.svg.png'>";
-		} else if (newCard.getAttribute('data-card') === 'jack'){
+			newCard.innerHTML = "<img src='assets/queen_of_hearts.png'>";
+		} else if (newCard.getAttribute('data-card') === 'jack') {
 			// jack
-			newCard.innerHTML = "<img src='https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Jack_of_hearts2.svg/2000px-Jack_of_hearts2.svg.png'>";
+			newCard.innerHTML = "<img src='assets/jack_of_hearts.png'>";
 		} else if (newCard.getAttribute('data-card') === 'ace') {
 			// ace
-			newCard.innerHTML = "<img src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/Playing_card_heart_A.svg/2000px-Playing_card_heart_A.svg.png'>";
+			newCard.innerHTML = "<img src='assets/ace_of_hearts.png'>";
 		} else if (newCard.getAttribute('data-card') === 'ten') {
 			// ten
-			newCard.innerHTML = "<img src='https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Playing_card_heart_10.svg/2000px-Playing_card_heart_10.svg.png'>"
+			newCard.innerHTML = "<img src='assets/10_of_hearts.png'>"
 		} else {
 			// joker
-			newCard.innerHTML = "<img src='http://monolithicgames.com/wp-content/uploads/2014/06/45-Joker.png'>"
+			newCard.innerHTML = "<img src='assets/joker.png'>"
 
 		}
 		// add new element to the element with id of 'game-board'
 		document.getElementById('game-board').appendChild(newCard);
 		newCard.addEventListener('click', isTwoCards)
-		}
+	}
 }
 
 var isTwoCards = function() {
 	// if card is not flipped (not showing card image), proceed. Nothing will happen if card is already flipped
-	if (this.innerHTML === ""){
+	if (this.innerHTML === "") {
 		//increment card
 		flippedCards += 1;
-		console.log (flippedCards);
 	  // add card to array of cards in play
 	  cardsInPlay.push(this.getAttribute('data-card'));
 		// show the card's image (flip face up)
-		console.log(this.getAttribute('data-card'));
 		if (this.getAttribute('data-card') === 'king') {
 			// king
-			this.innerHTML = "<img src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/06/King_of_hearts2.svg/2000px-King_of_hearts2.svg.png'>";
+			this.innerHTML = "<img src='assets/king_of_hearts.png'>";
 		} else if (this.getAttribute('data-card') === 'queen') {
 			// queen
-			this.innerHTML = "<img src='https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/Queen_of_hearts2.svg/2000px-Queen_of_hearts2.svg.png'>";
-		} else if (this.getAttribute('data-card') === 'jack'){
+			this.innerHTML = "<img src='assets/queen_of_hearts.png'>";
+		} else if (this.getAttribute('data-card') === 'jack') {
 			// jack
-			this.innerHTML = "<img src='https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Jack_of_hearts2.svg/2000px-Jack_of_hearts2.svg.png'>";
-		} else if (this.getAttribute('data-card') === 'ace'){
+			this.innerHTML = "<img src='assets/jack_of_hearts.png'>";
+		} else if (this.getAttribute('data-card') === 'ace') {
 			// ace
-			this.innerHTML = "<img src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/Playing_card_heart_A.svg/2000px-Playing_card_heart_A.svg.png'>";
+			this.innerHTML = "<img src='assets/ace_of_hearts.png'>";
 		} else if (this.getAttribute('data-card') === 'ten') {
 			// ten
-			this.innerHTML = "<img src='https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Playing_card_heart_10.svg/2000px-Playing_card_heart_10.svg.png'>"
+			this.innerHTML = "<img src='assets/10_of_hearts.png'>"
 		} else {
 			// joker
-			this.innerHTML = "<img src='http://monolithicgames.com/wp-content/uploads/2014/06/45-Joker.png'>"
+			this.innerHTML = "<img src='assets/joker.png'>"
+
 		}
-	  // if you have two cards in play, check for a match
+	  	// if you have two cards in play, check for a match
 	  	if (cardsInPlay.length === 2) {
 
 	    // pass the cardsInPlay as an argument to the isMatch function
@@ -89,7 +90,7 @@ var isMatch = function(cardsInPlay) {
 	// if cards are a match
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		// display message that cards match
-		alert("That's a match - +1 internets for you!");
+		alert("That's a match... +1 internets for you!");
 		// increment score total by 1
 		score += 1;
 		// update score element with new score
@@ -101,15 +102,31 @@ var isMatch = function(cardsInPlay) {
 	// if all cards have been flipped
 	if (flippedCards === cards.length) {
 		// tell player the game is over, and show score
-		alert("Game Over! Your score is: " + score);
+		alert("Game Over! You've won " + score + " Internets!");
 	}
 }
 
-// Run function to create cards. Cards are face up at first in order for player to memorize pattern
-createBoard(cards.length);
+var resetScore = function() {
+	// set score variable back to 0
+	score = 0;
+	// set score display to equal variable, which is 0
+	document.getElementById('score').innerHTML = "Score: " + score + " Internets";
+};
+
+// clicking the New Game button reschuffles board and flips cards faceup
+document.getElementById("new").addEventListener("click", function() {
+	// empty the game board
+	document.getElementById('game-board').innerHTML = "";
+	// repopulate the board
+	createBoard(cards.length);
+	//change button label
+	document.getElementById('reset').innerHTML = "Begin!"
+	// set score back to 0
+	resetScore()
+});
 
 // Clicking reset button flips all cards face down and sets score to 0
-document.getElementById("reset").addEventListener("click", function(){
+document.getElementById("reset").addEventListener("click", function() {
 	//find all cards on the board
 	var cardsInPlay = document.getElementsByClassName('card');
     //loop through each card on the board
@@ -117,11 +134,132 @@ document.getElementById("reset").addEventListener("click", function(){
    		//remove image from card (flip face down)
     	cardsInPlay[i].innerHTML = "";
     }
-    // set score variable back to 0
-	score = 0;
-	// set score display to equal variable, which is 0
-	document.getElementById('score').innerHTML = "Score: " + score + " Internets";
+	// set number of flipped cards back to 0
+	flippedCards = 0;
 	// change button label after clicked
-	document.getElementById('reset').innerHTML = "Flip and Reset";
+	document.getElementById('reset').innerHTML = "Reset";
 });
 
+// blackjack
+
+// function to create deck of cards to be used for playing
+var createDeck = function() {
+	// create array with each card type Ace through King
+	var names = ['ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'jack', 'queen', 'king'];
+	// create array for each suit
+	var suits = ['hearts','diamonds','spades','clubs'];
+	// counter to be used to count number of cards
+	var cnt = 0;
+	// for each suit
+	for (var i = 0; i < suits.length; i++) {
+		// and for each card type for each suit
+		for (var j = 0; j < names.length; j++) {
+			var value = 10; // variable for card value to be used for adding up hand values. Defaults to 10
+			// if the card is 2-10, set the value to equal the card type. Otherwise, the value remains defaulted at 10 for face cards
+			if (names[j] > 0) {
+				value = names[j];
+			}
+			// add card name, suit, image name, and value to deck array
+			blackjackCards[cnt++] = [names[j], suits[i], names[j] + "_of_" + suits[i] + ".png", value];
+			}
+		}
+		return blackjackCards;
+	}
+
+// deal two cards to player
+var blackjackDeal = function() {
+	// shuffle cards with a sort function I found somewhere on the internet
+	blackjackCards.sort(function(a, b){return 0.5 - Math.random()});
+	// For each card
+	for (var i = 0; i < 2; i++){
+		//Add a new div element
+		var newCard = document.createElement('div');
+		// with class of 'card'
+		newCard.className = 'card';
+		// add card image
+		newCard.innerHTML = "<img src='assets/" + blackjackCards[i][2] + "'>";
+		// add new element to the element with id of 'game-board'
+		document.getElementById('game-board').appendChild(newCard);
+		// Add card value to score total
+		if (blackjackCards[i][0] === 'ace') {
+			cardTotal += 11;
+		} else {
+			cardTotal += blackjackCards[i][3];
+		}
+		flippedCards += 1;
+		isTwentyOne();
+	}
+	// display total of cards in play
+	document.getElementById('blackjackScore').innerHTML = "Total: " + cardTotal;
+	// hide deal button
+	document.getElementById('deal').className = "hidden"
+	// show hit me button
+	document.getElementById('hit_me').className = ""
+}
+
+var isTwentyOne = function() {
+	// if total of cards in play equals 21
+	if (cardTotal === 21) {
+		// player wins hand
+		alert("Blackjack! You win!");
+		document.getElementById("hit_me").className = "hidden";
+		document.getElementById("deal").className = "";
+	// otherwise, if total of cards in play is greater than 21
+	} else if (cardTotal > 21) {
+		// player loses hand
+		alert ("Bust. Bummer :(")
+		document.getElementById("hit_me").className = "hidden";
+		document.getElementById("deal").className = "";
+	}
+}
+
+// when memory game button is clicked
+document.getElementById("memory").addEventListener("click", function() {
+	// remove hidden class from memory game section to show it
+	document.getElementById("memory_game").className = "";
+	// add hidden class to game selection section to hide it
+	document.getElementById("select_game").className = "hidden";
+	// Run function to create cards. Cards are face up at first in order for player to memorize pattern
+	createBoard(cards.length);
+});
+
+//when blackjack button is clicked
+document.getElementById("blackjack").addEventListener("click", function() {
+	// remove hidden class from blackjack game section to show it
+	document.getElementById("blackjack_game").className = "";
+	// add hidden class to game selection section to hide it
+	document.getElementById("select_game").className = "hidden";
+
+});
+
+//when blackjack deal button is clicked
+document.getElementById("deal").addEventListener("click", function() {
+	// make sure there are no cards on the board
+	document.getElementById("game-board").innerHTML = "";
+	// reset card total to 0 in case it previously had a value
+	cardTotal = 0;
+	//run blackjackDeal function
+	createDeck();
+	//deal two cards to playdr
+	blackjackDeal();
+});
+
+//when Hit Me button is clicked
+document.getElementById("hit_me").addEventListener("click", function() {
+	// Increment flipped cards counter by 1
+	flippedCards += 1;
+	// Create a new div element
+	var newCard = document.createElement('div');
+	// with class of 'card'
+	newCard.className = 'card';
+	// add card image
+	newCard.innerHTML = "<img src='assets/" + blackjackCards[flippedCards][2] + "'>";
+	// add new element to the element with id of 'game-board'
+	document.getElementById('game-board').appendChild(newCard);
+	// Add card value to score total
+	cardTotal += blackjackCards[flippedCards][3];
+	// display updated score
+	document.getElementById('blackjackScore').innerHTML = "Total: " + cardTotal;
+	// check score
+	isTwentyOne();
+});
